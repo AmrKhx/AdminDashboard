ab52be9973b5d34bd1b8dbef00a632728761724d
AmrKhx <102529176+AmrKhx@users.noreply.github.com>
2023-05-03 19:02:54 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce_admin_tut/provider/app_provider.dart';
+import 'package:ecommerce_admin_tut/rounting/route_names.dart';
+import 'package:ecommerce_admin_tut/rounting/router.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'locator.dart';
+
+void main() {
+  setupLocator();
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: AppProvider.init()),
+      ],
+      child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      onGenerateRoute: generateRoute,
+      initialRoute: LoginRoute,
+    );
+  }
+}
